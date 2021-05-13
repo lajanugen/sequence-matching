ddc3824ef37305bff4cd5f6cd97bd72d9ab3cf29
Lajanugen Logeswaran <llajan29@gmail.com>
2021-05-13 20:33:27 +0000

    Add left, right flanking specification and clean up headers

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 Instructions for running code
 
-`python find_matches.py <query string> <fasta file path> <number of flanking chars> <output file>`
-
 Example
 
-`python find_matches.py ctaac GCF_011064685.1_ZJU_Sfru_1.0_genomic.fna 0 output.txt`
+`
+python find_matches.py \
+  --query ctaac \
+  --input GCF_011064685.1_ZJU_Sfru_1.0_genomic.fna \
+  --num-flank-left 0 \
+	--num-flank-right 0 \
+	--output output.txt \
+	--clean-headers
+`
 
 Example output
 
-\>NC\_049710.1 Spodoptera frugiperda isolate Faw-zju chromosome 1, ZJU\_Sfru\_1.0, whole genome shotgun sequence-3-28-+-ctaacc  
+\>NC\_049710.1-3-28-+-ctaacc  
 aaaccacacactaacccacccccccc  
-\>NC\_049710.1 Spodoptera frugiperda isolate Faw-zju chromosome 1, ZJU\_Sfru\_1.0, whole genome shotgun sequence-28-57-+-ctaacctaac  
+\>NC\_049710.1-28-57-+-ctaacctaac  
 ccccacctaactaacctaacacctaaccta  
 ...
--- find_matches.py
+++ find_matches.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 
 def read_fasta(fname):
   headers, sequences = [], []
@@ -34,7 +35,7 @@ def longest_matching_subsequence(s1, s2):
     count += 1
   return count
 
-def find_matches(sequence, query, N):
+def find_matches(sequence, query):
   matches = []
   query_len = len(query)
   pos = sequence.find(query) 
@@ -51,22 +52,38 @@ def reverse_complement(query):
   c_tgt = ['t', 'g', 'a', 'c']
   mapping = dict(zip(c_src, c_tgt))
   return ''.join([mapping[c] for c in query][::-1])
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--query', type=str, default="", help="Query string")
+  parser.add_argument('--input', type=str, default="", help="Fasta file path")
+  parser.add_argument('--num-flank-left', type=int, default=0, help="Number of flanking chars on left")
+  parser.add_argument('--num-flank-right', type=int, default=0, help="Number of flanking chars on right")
+  parser.add_argument('--output', type=str, default="", help="Path to output file")
+  parser.add_argument('--clean-headers', action='store_true', default=False)
+  args = parser.parse_args()
   
-query = sys.argv[1].lower()
-if 'n' in query:
-  raise ValueError('Invalid query sequence: has character N')
-
-headers, sequences = read_fasta(sys.argv[2])
-num_sequences = len(headers)
-Nflank = int(sys.argv[3])
-output_file = sys.argv[4]
-
-with open(output_file, 'w') as f:
-  for header, sequence in zip(headers, sequences):
-    for sign, query_version in zip(('+', '-'), (query, reverse_complement(query))):
-      matches = find_matches(sequence, query_version, Nflank)
-      for start_pos, end_pos in matches:
-        matching_query = sequence[start_pos: end_pos]
-        matching_sequence = sequence[start_pos-Nflank: end_pos+Nflank]
-        f.write('%s-%d-%d-%s-%s\n' % (header, start_pos-Nflank+1, end_pos+Nflank, sign, matching_query))
-        f.write('%s\n' % matching_sequence)
+  query = args.query.lower()
+  if 'n' in query:
+    raise ValueError('Invalid query sequence: has character N')
+  
+  headers, sequences = read_fasta(args.input)
+  num_sequences = len(headers)
+  Nflank_left = args.num_flank_left
+  Nflank_right = args.num_flank_right
+  output_file = args.output
+  
+  with open(output_file, 'w') as f:
+    for header, sequence in zip(headers, sequences):
+      if args.clean_headers:
+        header = header.split()[0]
+      for sign, query_version in zip(('+', '-'), (query, reverse_complement(query))):
+        matches = find_matches(sequence, query_version)
+        left, right = Nflank_left, Nflank_right
+        if sign == '-':
+          left, right = right, left
+        for start_pos, end_pos in matches:
+          matching_query = sequence[start_pos: end_pos]
+          matching_sequence = sequence[start_pos-left: end_pos+right]
+          f.write('%s-%d-%d-%s-%s\n' % (header, start_pos-left+1, end_pos+right, sign, matching_query))
+          f.write('%s\n' % matching_sequence)
